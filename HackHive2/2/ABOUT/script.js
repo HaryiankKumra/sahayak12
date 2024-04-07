b4b544544a4e086ebd2a1cde52615fbcc7dde969
HaryiankKumra <144940685+HaryiankKumra@users.noreply.github.com>
2024-04-07 12:22:01 +0530

    Delete HackHive2 directory

--- HackHive2/2/ABOUT/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const loginBtn = document.getElementById('loginBtn');
-    loginBtn.addEventListener('click', function() {
-      // Implement login functionality here
-      console.log('Login button clicked');
-    });
-  });
-  
